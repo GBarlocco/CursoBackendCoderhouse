7ec9827a1e3e38bb391cdf4e1228b2cb4e06c157
[GastonBarlocco] <[barlocco@hotmail.es]>
2022-04-21 20:45:41 -0300

    Desafio 1 Clases

---
+++ src/Desafios/Desafio1Clases.js
@@ -0,0 +1,61 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+
+    getFullName() {
+        return `Nombre: ${this.nombre} Apellido: ${this.apellido}`;
+    }
+
+    addMascota(newMascota) {
+        this.mascotas.push(newMascota);
+    }
+
+    countMascotas() {
+        return this.mascotas.length;
+    }
+
+    addBook(nameBook, nameAuth) {
+        this.libros.push({ nombre: nameBook, autor: nameAuth });
+    }
+
+    getBookNames() {
+        let bookName = this.libros.map((book) => {
+            return book.nombre;
+        })
+
+        return bookName;
+    }
+}
+
+let libros1 = [
+    {
+        nombre: "Nombre Libro 1",
+        autor: "autor1"
+    },
+    {
+        nombre: "Nombre Libro 2",
+        autor: "autor2"
+    }
+];
+
+let mascotas1 = [`mascota1`, `mascota2`, `mascota3`];
+
+let usuario1 = new Usuario(`Gastón`, `Barlocco`, libros1, mascotas1);
+
+console.log(usuario1.getFullName());
+
+console.log(`Total de mascotas`, usuario1.countMascotas());
+
+usuario1.addMascota(`mascota4`);
+
+console.log(`Total de mascotas`, usuario1.countMascotas());
+
+console.log(`libros`, usuario1.getBookNames());
+
+usuario1.addBook(`Nombre Libro nuevo`, `autor nuevo`);
+
+console.log(`libros`, usuario1.getBookNames());
