e96549d62b63508a5a4f3bb95454bd887e558bbe
[GastonBarlocco] <[barlocco@hotmail.es]>
2022-05-05 21:28:08 -0300

    Modificación ruta de ejercicio C3_E2

--- src/EjerciciosClase/Clase3/C3_E2.js
+++ src/EjerciciosClase/Clase3/C3_E2.js
@@ -3,14 +3,14 @@ const fs = require(`fs`);
 const date = Date().toLocaleString();
 
 try {
-    fs.writeFileSync(`./fyh.txt`, date);
+    fs.writeFileSync(`./src/EjerciciosClase/Clase3/fyh.txt`, date);
 
 } catch (err) {
     console.error(err);
 }
 
 try {
-    const date = fs.readFileSync(`./fyh.txt`, `utf-8`);
+    const date = fs.readFileSync(`./src/EjerciciosClase/Clase3/fyh.txt`, `utf-8`);
     console.log(`La fecha actual es ${date}`);
 } catch (err) {
     console.error(err);
